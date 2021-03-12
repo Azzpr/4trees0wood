d4f8e3b575571b4069aadfd0d1df6a02c2b6e39e
Mathias <mathiasandre1998@gmail.com>
2021-03-12 22:44:13 +0100

    Worked on the projects page

--- client/src/Pages/Projects/Projects.js
+++ client/src/Pages/Projects/Projects.js
@@ -8,11 +8,34 @@ import Navbar from '../../components/Navar/Navbar';
 import Footer from '../../components/Footer/Footer';
 const Projects = () => {
     return (
-        <div>
+        <body>
             <Navbar />
-        
+            <div className="projects-container">
+                {/*project item 1 */}
+                <div className="project-item">
+                    <div className="thumbnail-image">
+                        <img src="/images/flappybirdProjectThumbnail.jpg" alt="Preview image" />
+                    </div>
+                    <div className="name">
+                        <a href="https://github.com/Azzpr/FlappyBird-SFML">
+                            <h1 id="name">Flappybird</h1>
+                        </a>
+                    </div>
+                    <div className="small-description">
+                        <p>A game jam project, made using C++ with the library SFML(Simple fast media library)</p>
+                    </div>
+                    <div className="github-url"> 
+                        <a href="https://github.com/Azzpr/FlappyBird-SFML">
+                            <span class="material-icons">
+                                code
+                        </span>
+                        </a>
+                    </div>
+                </div>
+                {/*project item 1 */}
+            </div>
             <Footer />
-        </div>
+        </body>
     )
 }
 export default Projects;
